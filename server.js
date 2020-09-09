48769c188dd974dd4d075b37c818c0fc710dd4d7
celcito <celso_ricardo_25@hotmail.com>
2020-09-09 12:33:55 -0300

    settings
    settings

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require ('express')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const cors  = require('cors')
 //Init app
 const app = express()
+
 //Init db
 const uri = 'mongodb+srv://smartstaff:dIgNoRKQ5tKLsp1Q@cluster0.xfbdl.mongodb.net/smartstaff?retryWrites=true&w=majority'
 
-mongoose.Promise = global.Promise
 mongoose.connect( uri, {useNewUrlParser: true, useUnifiedTopology: true})
+app.use(express.json())
+
+app.use(cors())
+
 requireDir('./src/models')
 app.use('/api',require('./src/routes'))
 app.listen(3005)
+module.exports = app
--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,6 @@ const express = require('express')
 const routes = express.Router()
 const CustomersController = require('./controllers/CustomersController')
 routes.get("/customers",CustomersController.index)
+
+
 module.exports = routes
